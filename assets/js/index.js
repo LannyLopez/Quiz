2da3b2b234ea23508a7a5f6af6b9540e4efa5d87
lanny lopez <lannylynnlopez89@gmail.com>
2021-10-14 00:54:37 -0600

    forgot to push again added java sript and timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,40 @@
 var starterForm = document.getElementById("foam")
 var starterBtn = starterForm.querySelector("#btn")
+var otherBtn = document.getElementsByClassName("other")
 var qsTwo = document.getElementById("foam2")
+var qsThree = document.getElementById("foam3")
+var qsWrong = document.getElementById("wrongFoam")
 
 starterBtn.addEventListener("click", function(){
     starterForm.style.display = "none"
     qsTwo.style.display = "inherit"
+    
+    startTimer()
 })
+
+
+otherBtn.addEventListener("click",function(){
+    qsTwo.style.display = "none"
+    qsThree.style.display = "inherit"
+})
+
+function startTimer() {
+    const timeDisplay = document.getElementById("timer-countdown");
+    let timeRemaining = 90;
+  
+    intervalID = setInterval(function () {
+      timeRemaining--;
+  
+      timeDisplay.innerText = timeRemaining + "s";
+  
+      if (timeRemaining <= 0) {
+        stopTimer();
+      }
+    }, 1000);
+  }
+  
+  function stopTimer() {
+    clearInterval(intervalID);
+    document.querySelector('main').innerHTML = '<h1>Time is up!</h1>';
+  }
+  
